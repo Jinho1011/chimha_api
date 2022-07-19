40dfd372987ae16a828e3982f7900c960a32c5f9
jinho1011 <gino9940@gmail.com>
2022-07-20 00:42:35 +0900

    ✨ feat: add user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('User')
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+}
